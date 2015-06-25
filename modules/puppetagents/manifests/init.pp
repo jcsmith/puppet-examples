5a178bb06269701de594e34a468a9ae3fc96d71e
Josh Smith <jsmith@mail.wvnet.edu>
2015-06-24 20:19:09 -0400

    puppetagents module

---
+++ modules/puppetagents/manifests/init.pp
@@ -0,0 +1,18 @@
+class puppetagents {
+
+  package { 'puppet' :
+    ensure => latest,
+  }
+
+ host { 'puppet' :
+   ensure => present,
+   ip     => '192.168.122.62',
+   notify => Service['puppet'],
+ }
+
+ service { 'puppet' :
+   ensure => running,
+   enable => true,
+ }
+}
+
